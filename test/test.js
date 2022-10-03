38870c901a26310ebd6ace47c65ecfdcf8ba1bb5
James Forbes <james.a.forbes@gmail.com>
2022-10-03 23:17:48 +1100

    Musing on what components are...

--- test/test.js
+++ test/test.js
@@ -124,6 +124,128 @@ test('nested computations', t => {
     t.end()
 })
 
+test('data setter fn', t => {
+    let a = S.data(1)
+
+    let renders = 0;
+    let c = S.computation(() => {
+        renders++
+        a()
+    })
+
+    t.equals(renders, 1, '1 render for initial value')
+
+    a( x => x + 1 )
+
+    t.equals(renders, 2, '1 write = 1 render')
+    t.equals(a(), 2, 'fn setter works')
+    t.end()
+})
+
+test('components?', t => {
+    S.stats.ticks = 0
+
+    function Button({ onclick }, text){
+        let redraws = 0
+        let out = {
+            get redraws(){
+                return redraws
+            },
+            view: S.computation(() => {
+                redraws++
+                return text
+            }),
+            onclick
+        }
+        return out
+    }
+
+    function ShowCount({ count }){
+        
+        let redraws = 0
+        let out = {
+            get redraws(){
+                return redraws
+            },
+            view: S.computation(() => {
+                redraws++
+                return 'The count is ' + count()
+            })
+        }
+        return out;
+    }
+
+    function Counter(){
+
+        let count = S.data(0)
+
+        let redraws = 0
+        let out = {
+            get redraws(){
+                return redraws
+            },
+            count,
+            view: S.computation(() => {
+                redraws++
+                return [
+                    ShowCount({ count }),
+                    Button({ onclick: () => count( x => x + 1) }, '+'),
+                    Button({ onclick: () => count( x => x - 1) }, '-')
+                ]
+            })
+        }
+
+        return out;
+    }
+
+    let counter = Counter()
+
+    let showCount = () => counter.view()[0]
+    let inc = () => counter.view()[1]
+    let dec = () => counter.view()[2]
+    
+    t.equals(showCount().view(), 'The count is 0', 'ShowCount')
+    t.equals(inc().view(), '+', '+')
+    t.equals(dec().view(), '-', '-')
+    t.equals(S.stats.ticks, 0, 'No writes no ticks')
+
+    t.equals(counter.redraws, 1, 'counter.redraws = 1')
+    t.equals(showCount().redraws, 1, 'showCount.redraws = 1')
+    t.equals(inc().redraws, 1, 'inc.redraws = 1')
+    t.equals(dec().redraws, 1, 'dec.redraws = 1')
+
+    inc().onclick()
+
+    t.equals(showCount().view(), 'The count is 1', 'ShowCount')
+    t.equals(inc().view(), '+', '+')
+    t.equals(dec().view(), '-', '-')
+    t.equals(S.stats.ticks, 1, '1 write, 1 tick')
+
+    t.equals(counter.redraws, 1, 'counter.redraws = 1')
+    t.equals(showCount().redraws, 2, 'showCount.redraws = 2')
+    t.equals(inc().redraws, 1, 'inc.redraws = 1')
+    t.equals(dec().redraws, 1, 'dec.redraws = 1')
+
+    S.batch(() => {
+        dec().onclick()
+        dec().onclick()
+        dec().onclick()
+        dec().onclick()
+    })
+
+    t.equals(showCount().view(), 'The count is -3', 'ShowCount')
+    t.equals(inc().view(), '+', '+')
+    t.equals(dec().view(), '-', '-')
+    t.equals(S.stats.ticks, 2, '2 write, 2 tick')
+
+    t.equals(counter.redraws, 1, 'counter.redraws = 1')
+    t.equals(showCount().redraws, 3, 'showCount.redraws = 3')
+    t.equals(inc().redraws, 1, 'inc.redraws = 1')
+    t.equals(dec().redraws, 1, 'dec.redraws = 1')
+
+    t.end()
+})
+
 test('define data inside a computation', t => {
     S.stats.ticks = 0
 
