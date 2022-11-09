865bec918d68fe9a83461c3d51387c6ebfe276dd
James Forbes <james.a.forbes@gmail.com>
2022-11-09 23:07:24 +1100

    Use table/row naming internally

--- lib/store.ts
+++ lib/store.ts
@@ -9,11 +9,11 @@ type Store<T> = {
 	sampleAll(): T[];
 	read(): T;
 	readAll(): T[];
-	setState(f: (x?: T) => T): void;
+	setState(f: (row?: T) => T): void;
 	getReadStream: () => S.Computation<T[]>;
 	focus<U>(
-		get: (x: T) => U[] | [],
-		set: (state: T, update: ( (x:U) => U) ) => T,
+		get: (row: T) => U[] | [],
+		set: (state: T, update: ( (row:U) => U) ) => T,
 		...dependencies: S.Computation<any>[]
 	): Store<U>;
 
@@ -25,11 +25,11 @@ type Store<T> = {
 	// whereUnnested: ReturnType<Store<T>["unnest"]>["where"]
 	whereUnnested: typeof whereUnnested
 
-	filter( f: ( (x:T) => boolean) ): Store<T>
+	filter( f: ( (row:T) => boolean) ): Store<T>
 };
 
 type NotifyMap<T> = WeakMap<Store<T>, (
-	(f: ((x:T) => T)) => void
+	(f: ((row:T) => T)) => void
 )>;
 
 const notify: NotifyMap<any> = new WeakMap();
@@ -37,20 +37,20 @@ const notify: NotifyMap<any> = new WeakMap();
 const Instances: Map<string, Store<any>> = new Map()
 
 function prop<T, K extends keyof T>(this: Store<T>, key: keyof T) : Store<T[K]> {
-	return createChildStore(x => [x[key]], (parent, update) => ({ ...parent, [key]: update(parent[key])}), this) as Store<T[K]>;
+	return createChildStore(row => [row[key]], (parent, update) => ({ ...parent, [key]: update(parent[key])}), this) as Store<T[K]>;
 }
 function unnest<T>(this: Store<T>){
 	return createChildStore(
-		xs => (xs as any[]),
-		(list, update) => (list as any[]).map( x => update(x) ) as T,
+		arrayRow => (arrayRow as any[]),
+		(arrayRow, update) => (arrayRow as any[]).map( unnestedRow => update(unnestedRow) ) as T,
 		this,
 	)
 }
 
-function filter<T>(this: Store<T>, f: (x:T) => boolean) {
+function filter<T>(this: Store<T>, f: (row:T) => boolean) {
 	return createChildStore(
-		x => f(x) ? [x] : [],
-		(x, update) => (f(x) ? update(x) : x ),
+		row => f(row) ? [row] : [],
+		(row, update) => (f(row) ? update(row) : row ),
 		this,
 	)
 }
@@ -63,18 +63,18 @@ function where<
 >(this: Store<T>, record: R) : Store<T> {
 	const anyRecord = record as Record<string, any>;
 	return createChildStore(
-		x => Object.entries(anyRecord).flatMap(
-				([k,v]) => (x as any)[k] === v() ? [x] : []
+		row => Object.entries(anyRecord).flatMap(
+				([k,v]) => (row as any)[k] === v() ? [row] : []
 		) as any,
-		(object, update) => {
+		(row, update) => {
 			if ( 
 				Object.entries(anyRecord).every(
-					([k,v]) => (object as any)[k] === v()
+					([k,v]) => (row as any)[k] === v()
 				)	
 			) {
-				return update(object as any) as any as T
+				return update(row as any) as any as T
 			}
-			return object
+			return row
 		},
 		this,
 	)
@@ -89,17 +89,17 @@ function whereUnnested <T, TT extends Unnest<T>, K extends keyof TT, V extends T
 
 	const anyRecord = record as Record<string, any>;
 	return createChildStore(
-		xs => (xs as any[]).filter( x =>  
+		arrayRow => (arrayRow as any[]).filter( unnestedRow =>  
 			Object.entries(anyRecord).every(
-				([k,v]) => x[k] === v()
+				([k,v]) => unnestedRow[k] === v()
 			)
 		) as any,
-		(list, update) => (list as any[]).map( x => 
+		(arrayRow, update) => (arrayRow as any[]).map( unnestedRow => 
 			Object.entries(anyRecord).every(
-				([k,v]) => x[k] === v()
+				([k,v]) => unnestedRow[k] === v()
 			)
-			? update(x)
-			: x
+			? update(unnestedRow)
+			: unnestedRow
 		) as any,
 		this,
 	)
@@ -107,17 +107,17 @@ function whereUnnested <T, TT extends Unnest<T>, K extends keyof TT, V extends T
 
 function focus<T, U> (
 	this: Store<T>
-	, getter: (x: T) => U[] | []
-	, setter: (state: T, update: ( (x:U) => U) ) => T
+	, getter: (row: T) => U[] | []
+	, setter: (state: T, update: ( (row:U) => U) ) => T
 )  {
 	return createChildStore(getter, setter, this);
 }
 
-export function createStore<T>(xs: T[]): Store<T> {
-	const stateStream = S.data(xs);
+export function createStore<T>(table: T[]): Store<T> {
+	const stateStream = S.data(table);
 	
 	const setState: Store<T>["setState"] = (f) => {
-		stateStream(S.sample(stateStream).map( x => f(x) ))
+		stateStream(S.sample(stateStream).map( row => f(row) ))
 	};
 
 	let store : Store<T> = {
@@ -138,7 +138,7 @@ export function createStore<T>(xs: T[]): Store<T> {
 	notify.set(store, (f) => {
 		stateStream( 
 			S.sample(stateStream).map( 
-				x => f(x)
+				row => f(row)
 			)
 		);
 	});
@@ -153,13 +153,13 @@ function createChildStore<Parent, Child>(
 ): Store<Child> {
 
 	const read$ = U.dropRepeatsWith(
-		U.map( (xs) => {
-			return xs.flatMap( x => getter(x) )
+		U.map( (results) => {
+			return results.flatMap( row => getter(row) )
 		}, parentStore.getReadStream()),
-		(xs, ys) => xs.length === ys.length	&& xs.every( (x,i) => x === ys[i])
+		(tableA, tableB) => tableA.length === tableB.length	&& tableA.every( (rowA,i) => rowA === tableB[i])
 	);
 
-	const setState = (f: (x?: Child) => Child) => {
+	const setState = (f: (row?: Child) => Child) => {
 		notify.get(parentStore)!(
 			(parent: Parent) => setter(parent, f)
 		);
